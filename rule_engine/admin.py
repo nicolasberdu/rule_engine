0b8984e075d537a8159712778023a6b54fd52f70
Nicolas <berdunicolas@gmail.com>
2024-01-16 12:45:46 -0300

    Create Models and register

--- rule_engine/admin.py
+++ rule_engine/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, ActivationLink, Rule
 
 admin.site.register(User)
+admin.site.register(ActivationLink)
+admin.site.register(Rule)
--- rule_engine/models.py
+++ rule_engine/models.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.utils.crypto import get_random_string
+from django.utils.timezone import now
 
 class User(AbstractUser, PermissionsMixin):
 
     username = models.CharField(max_length=765, null=False, unique=True)
+    password = models.CharField(max_length=128, verbose_name='password', null=True, default=None, blank=True)
     email = models.EmailField(null=False, unique=True)
     failed_logins = models.IntegerField(null=False, default=0)
     is_blocked = models.BooleanField(default=False)
@@ -12,3 +17,26 @@ class User(AbstractUser, PermissionsMixin):
     
     def __str__(self):
         return self.username
+    
+class Rule(models.Model):
+    attribute = models.CharField(max_length=40, unique=True, blank=False)
+    description = models.CharField(max_length=100, unique=True, blank=False)
+    value = models.CharField(max_length=40, null=True, default=None)
+    update_at = models.DateTimeField(default=now)
+
+    
+
+class ActivationLink(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    key = models.CharField(primary_key=True, max_length=40, unique=True)
+    created_at = models.DateTimeField(default=now)
+
+    @classmethod
+    def generate_new_link(cls, user):
+        key = get_random_string(length=40)
+        activation_link = cls.objects.create(user=user, key=key)
+
+        return activation_link
+    
+    def __str__(self):
+        return self.key
